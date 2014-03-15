a3a73f1e04417673c0c299de749bfcf1ff9692e9
Barbie <barbie@missbarbell.co.uk>
2014-03-15 10:50:02 +0000

    exclude NA reports from the nopass RSS feed

--- lib/Labyrinth/Plugin/CPAN/Report.pm
+++ lib/Labyrinth/Plugin/CPAN/Report.pm
@@ -147,7 +147,7 @@ sub load_rss {
         my @reports;
         for my $row (sort {$b->{fulldate} <=> $a->{fulldate}} @$data) {
             next    if($row->{fulldate} lt $olddate); # ignore anything older than a year
-            next    if($nopass && $row->{state} =~ /PASS/i);
+            next    if($nopass && $row->{state} =~ /PASS|NA/i);
             push @reports, $row;
         }
 
